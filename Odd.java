545cc22dd289839c178ea4159c4d290ee5adb090
Kanaga2 <priyakanaga112@gmail.com>
2017-07-03 12:12:53 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,23 @@
+# Odd
+import java.io.*;
+import java.util.*;
+public class Odd
+{
+public static void main(String args[])
+{
+int n1,n2;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the limit in n1");
+n1=sc.nextInt();
+System.out.println("Enter the limit in n2");
+n2=sc.nextInt();
+if(n%2!=0)
+{
+System.out.println("print odd numbers between n1 to n2");
+}
+else
+{
+System.out.println("print even numbers");
+}
+}
+}
